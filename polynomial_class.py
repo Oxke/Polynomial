6693b56bc04d9cb6d001e5bb9a7b6a527f2e17bb
Oxke <oseaetobia@gmail.com>
2018-12-25 03:43:30 +0100

    New python math class!!!

---
+++ polynomial_class.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  Polynomial, another python math class, the last one maybe
+#  Copyright (C) 2018 Oxke
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  If you want to contact me, -> oseaetobia@gmail.com
+#
+#  Last modified: 12/25/18 3:41 AM
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  If you want to contact me, -> oseaetobia@gmail.com
+#
+#  Last modified: 12/25/18 3:40 AM
+
+__author__ = "Oxke"
+__contact__ = "oseaetobia@gmail.com"
+__copyright__ = "Copyright (C) 2018, Oxke"
+__license__ = "GNU GPLv3.0"  # Read the file LICENSE for more information
+__project__ = "Polynomial"
+__version__ = "v0.1"
+__date__ = "2018-12-25"
+
+
+class Polynomial:
+    def __init__(self):
+        pass
